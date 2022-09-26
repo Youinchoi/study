8dd16f999e7f88fa1c75050380090836f62f9f3e
youinchoi <112688146+Youinchoi@users.noreply.github.com>
2022-09-26 10:10:46 +0900

    Add files via upload

---
+++ Study/src/c_collection/aArrayListEx3.java
@@ -0,0 +1,28 @@
+package c_collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Iterator;
+
+public class aArrayListEx3 {
+
+	public static void main(String[] args) {
+	
+	
+	}
+
+}
+
+//----------------------------------------------------------
+class Student{
+	String name;
+	int age;
+	Student(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	public String toString() {
+		return name +"학생은 " +  age + "세 입니다.";
+	}
+}
